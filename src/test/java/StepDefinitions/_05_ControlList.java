3c8b556e73631c01b32e5108a717c3688f33b6c2
yasinn35 <kayayasin541@gmail.com>
2023-09-28 17:01:24 +0300

    Control of the number of products listed

---
+++ src/test/java/StepDefinitions/_05_ControlList.java
@@ -0,0 +1,40 @@
+package StepDefinitions;
+
+import Pages.YasinLocators;
+import Utilities.GWD;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.testng.Assert;
+
+public class _05_ControlList {
+    YasinLocators yl=new YasinLocators();
+
+    @Given("Navigate to Cleverppc_5")
+    public void navigateToCleverppc_5() {
+        GWD.getDriver().get("https://cleverppc.com/prestashop4/");
+    }
+    @When("User Clicks on the dresses category")
+    public void userClicksOnTheDressesCategory() {
+
+        yl.myClick(yl.dressesBtn);
+
+    }
+
+    @Then("Validating the number at the top of the list")
+    public void validatingTheNumberAtTheTopOfTheList() {
+        Assert.assertTrue(yl.ustList.getText().contains("5"));
+    }
+
+
+    @And("Validating the number at the buttom of the list")
+    public void validatingTheNumberAtTheButtomOfTheList() {
+        Assert.assertTrue(yl.altList.getText().contains("5"));
+    }
+
+    @Then("Check the number by counting list products")
+    public void checkTheNumberByCountingListProducts() {
+        Assert.assertTrue(yl.urunSayi.size()==5);
+    }
+}
